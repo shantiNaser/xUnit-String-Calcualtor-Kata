e82fd9f67e2e652c99c2f23587dcc1ccab2a35cd
NaserShanti <nasershanti654@gmail.com>
2021-08-14 21:01:52 +0300

    handle an unknown amount of numbers.

--- Test.Tests/Calculator.cs
+++ Test.Tests/Calculator.cs
@@ -11,12 +11,13 @@ namespace Test.Tests
         [InlineData("", 0)]
         [InlineData("2", 2)]
         [InlineData("2,3", 5)]
+        [InlineData("2,3,3", 8)]
+        [InlineData("2,3,3,2,5,6,4,3", 28)]
         public void ShouldReturnTheSumOfNumbersinString(string input, int expected)
         {
            Kata numberAsStr = new Kata();
            var actual = numberAsStr.add(input);
            Assert.Equal(expected, actual);
-
         }
 
 
